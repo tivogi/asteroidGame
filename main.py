468bb57f9da2492156366ceb11256fa9a2318620
tomy <tomy@DESKTOP-G6HBVJG.localdomain>
2025-12-11 13:14:53 +0000

    Created the player triangle (but with a circular hitbox)

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import pygame
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player( SCREEN_WIDTH / 2
+, SCREEN_HEIGHT / 2
+)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        miliseconds = clock.tick(60)
+        dt = miliseconds/1000
+        
+        
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
